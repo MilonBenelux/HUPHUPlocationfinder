ced94d39c0c426d94d99d790de67932924d18a84
MilonBenelux <karl.peeters@milonbenelux.com>
2026-09-23 20:20:06 +0200

    Integrating Kennismaking agendas

--- src/config/locations.js
+++ src/config/locations.js
@@ -7,19 +7,19 @@
  * (right-click the pin -> the numbers at the top of the context menu).
  */
 module.exports = [
-{ id: "loc-01", name: "HUPHUP Beveren-Waas", address: "Albert Panisstraat 89, 9120 Beveren-Waas", lat: 51.2132090388284, lng: 4.2549859765253 },
-{ id: "loc-02", name: "HUPHUP Brasschaat", address: "Kapelsesteenweg 382, 2930 Brasschaat", lat: 51.2929237629941, lng: 4.44375943922942 },
-{ id: "loc-03", name: "HUPHUP Dendermonde", address: "Oude Vest 88,9200 Dendermonde", lat: 51.0296978259531, lng: 4.10265819874751 },
-{ id: "loc-04", name: "HUPHUP Diepenbeek", address: "Nieuwstraat 167 bus 3,3590 Diepenbeek", lat: 50.9243119229091, lng: 5.45113699689164 },
-{ id: "loc-05", name: "HUPHUP Hoogstraten", address: "Minderhoutdorp 42a,2322 Hoogstraten", lat: 51.4161487133931, lng: 4.76352617885285 },
-{ id: "loc-06", name: "HUPHUP Kapellen", address: "Hoevensebaan 49,2950 Kapellen", lat: 51.3139160772005, lng: 4.42641738341073 },
-{ id: "loc-07", name: "HUPHUP Lanaken", address: "Heuvelstraat 57,3620 Lanaken", lat: 50.892343422814, lng: 5.65092750761083 },
-{ id: "loc-08", name: "HUPHUP Maasmechelen", address: "Rijksweg 263A bus 2,3630 Maasmechelen", lat: 50.9755386329794, lng: 5.70032114107372 },
-{ id: "loc-09", name: "HUPHUP Oostende", address: "Leffingestraat 40,8400 Oostende", lat: 51.2205062930681, lng: 2.91376524293345 },
-{ id: "loc-10", name: "HUPHUP Oudsbergen", address: "Gruitroderbaan 37,3670 Oudsbergen", lat: 51.0964511755, lng: 5.531561742 },
-{ id: "loc-11", name: "HUPHUP Schoten", address: "Venstraat 153 C,2900 Schoten", lat: 51.2507795795937, lng: 4.50598775590524 },
-{ id: "loc-12", name: "HUPHUP Tessenderlo", address: "Engsbergseweg 88 B,3980 Tessenderlo", lat: 51.0299755349365, lng: 5.05423832943665 },
-{ id: "loc-13", name: "HUPHUP Waterloo", address: "Chaussée de Bruxelles 88A,1410 Waterloo", lat: 50.7273117433373, lng: 4.39779356990309 },
-{ id: "loc-14", name: "HUPHUP Zandhoven", address: "Melkerijweg 1,2240 Zandhoven", lat: 51.2239050068163, lng: 4.67383549875286 },
-{ id: "loc-15", name: "HUPHUP Dendermonde Mechelsesteenweg", address: "Mechelsesteenweg 244,9200 Dendermonde", lat: 51.036537, lng: 4.123112 }
+{ id: "loc-01", name: "HUPHUP Beveren-Waas", address: "Albert Panisstraat 89, 9120 Beveren-Waas", lat: 51.2132090388284, lng: 4.2549859765253,calendar: "https://letsgo.huphup.be/widget/booking/27x5LOATvCCbLtr1HkeA"},
+{ id: "loc-02", name: "HUPHUP Brasschaat", address: "Kapelsesteenweg 382, 2930 Brasschaat", lat: 51.2929237629941, lng: 4.44375943922942,calendar: "https://letsgo.huphup.be/widget/booking/2TIK4cVBybCcyDsT7Hqh"},
+{ id: "loc-03", name: "HUPHUP Dendermonde - Mechelsesteenweg", address: "Mechelsesteenweg 244, 9200 Dendermonde", lat: 51.036537, lng: 4.123112,calendar: "https://letsgo.huphup.be/widget/booking/icfUHt8bSgLsPKkmgXCA"},
+{ id: "loc-04", name: "HUPHUP Dendermonde - Oude Vest", address: "Oude Vest 88, 9200 Dendermonde", lat: 51.0296978259531, lng: 4.10265819874751,calendar: "https://letsgo.huphup.be/widget/booking/BSAv35iDpnsLWIGSuNJU"},
+{ id: "loc-05", name: "HUPHUP Diepenbeek", address: "Nieuwstraat 167 bus 3, 3590 Diepenbeek", lat: 50.9243119229091, lng: 5.45113699689164,calendar: "https://letsgo.huphup.be/widget/booking/ZyGrXKw7HvIhDV9yjjXg"},
+{ id: "loc-06", name: "HUPHUP Hoogstraten", address: "Minderhoutdorp 42a, 2322 Hoogstraten", lat: 51.4161487133931, lng: 4.76352617885285,calendar: "https://letsgo.huphup.be/widget/booking/KWEF80KVJofv0pfFrrnp"},
+{ id: "loc-07", name: "HUPHUP Kapellen", address: "Hoevensebaan 49, 2950 Kapellen", lat: 51.3139160772005, lng: 4.42641738341073,calendar: "https://letsgo.huphup.be/widget/booking/F7qIORFXLcYFMUgguG7m"},
+{ id: "loc-08", name: "HUPHUP Lanaken", address: "Heuvelstraat 57, 3620 Lanaken", lat: 50.892343422814, lng: 5.65092750761083,calendar: "https://letsgo.huphup.be/widget/booking/IecTFrbHfXJjkb37p3Mm"},
+{ id: "loc-09", name: "HUPHUP Maasmechelen", address: "Rijksweg 263A bus 2, 3630 Maasmechelen", lat: 50.9755386329794, lng: 5.70032114107372,calendar: "https://letsgo.huphup.be/widget/booking/DTOXx4P73gH8Zgtt20Ra"},
+{ id: "loc-10", name: "HUPHUP Oostende", address: "Leffingestraat 40, 8400 Oostende", lat: 51.2205062930681, lng: 2.91376524293345,calendar: "https://letsgo.huphup.be/widget/booking/Tox48O1bNsjZlHLEhxIH"},
+{ id: "loc-11", name: "HUPHUP Oudsbergen", address: "Gruitroderbaan 37, 3670 Oudsbergen", lat: 51.0964511755, lng: 5.531561742,calendar: "https://letsgo.huphup.be/widget/booking/rTfBsuX8IhD9jUXoQQIF"},
+{ id: "loc-12", name: "HUPHUP Schoten", address: "Venstraat 153 C, 2900 Schoten", lat: 51.2507795795937, lng: 4.50598775590524,calendar: "https://letsgo.huphup.be/widget/booking/fifqsQZnbNb4oTvZDFAd"},
+{ id: "loc-13", name: "HUPHUP Tessenderlo", address: "Engsbergseweg 88 B, 3980 Tessenderlo", lat: 51.0299755349365, lng: 5.05423832943665,calendar: "https://letsgo.huphup.be/widget/booking/xAioaS1aFx0y5Y0Ff2sN"},
+{ id: "loc-14", name: "HUPHUP Waterloo", address: "Chaussée de Bruxelles 88A, 1410 Waterloo", lat: 50.7273117433373, lng: 4.39779356990309,calendar: "https://letsgo.huphup.be/widget/booking/CAnGtk44l84LDFyTBTZc"},
+{ id: "loc-15", name: "HUPHUP Zandhoven", address: "Melkerijweg 1, 2240 Zandhoven", lat: 51.2239050068163, lng: 4.67383549875286,calendar: "https://letsgo.huphup.be/widget/booking/7FfKOl3AsXE14CY9cNJi"}
 ];
